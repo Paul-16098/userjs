d25108433c02efb71d88d891e32eaa6040c54d9f
Paul-16098 <pl816098@gmail.com>
2025-01-14 13:31:16 +0800

    3.5.0.0 => 3.5.1.0

--- 69shuba auto 書簽/69shuba auto 書簽.user.js
+++ 69shuba auto 書簽/69shuba auto 書簽.user.js
@@ -3,7 +3,7 @@
 // ==UserScript==
 // @name         69shuba auto 書簽
 // @namespace    Paul-16098
-// @version      3.5.0.0
+// @version      3.5.1.0
 // @description  自動書籤,更改css,可以在看書頁找到作者連結
 // @author       Paul-16098
 // #tag 69shux.com
@@ -204,7 +204,7 @@ class BookManager {
     addBookcase() {
         const aid = this.data.Book.GetAid();
         const cid = this.data.Book.GetCid();
-        if (addbookcase.toString().includes("addbookcase.php")) {
+        if (!addbookcase.toString().includes("Ajax.Tip")) {
             addbookcase(aid, cid);
         }
         else {
