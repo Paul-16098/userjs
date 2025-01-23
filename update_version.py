7b9d658b6d434af576df63b6947cf9316fe44e13
Paul-16098 <pl816098@gmail.com>
2025-01-23 19:26:11 +0800

    新增 GitHub Actions 工作流程以自動化 TypeScript 構建和版本更新

---
+++ update_version.py
@@ -0,0 +1,27 @@
+from glob import glob
+import re
+
+
+def replace_version(ver: re.Match[str]) -> str:
+    # print(ver.groups())
+    new_ver = ver.group(1)
+    new_ver = re.sub(r"-edge\d+", "", new_ver)
+    return f"// @version      {new_ver}"
+
+
+# 查找所有 .user.ts 文件
+for file in glob("./*/*.user.ts"):
+    try:
+        with open(file, "r", encoding="utf-8") as f:
+            txt = f.read()
+
+        # 使用正则表达式替换版本号
+        new_txt = re.sub(pattern=r"// @version\s+((\d+\.?)+(-edge\d+)?)",
+                         repl=replace_version, string=txt)
+
+        # 写回文件
+        with open(file, "w", encoding="utf-8") as f:
+            f.write(new_txt)
+
+    except Exception as e:
+        print(f"Error processing file {file}: {e}")
